9c251b7876416f2448c5eeebcd9360d65bdee7d0
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2022-11-30 23:56:32 -0500

    add custom theme to tailwind and enable dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,21 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      raisinBlack: '#241f25',
+      lightGray: '#d5d4d5',
+      darkLiver: '#514e52',
+      silverFoil: '#afadb0',
+      lavenderGray: '#c3c3c9',
+      onyx: '#3c373d',
+      crayola: '#7a6bcf',
+      lightCrayola: '#978bd2',
+      darkCrayola: '#695fa3',
+      green: '#7bb64f',
+      lightGreen: '#80bc51',
+      darkGreen: '#6d994a',
+    }
   },
-  plugins: [],
+  plugins: [require('tailwindcss-global-dark')],
+  darkMode: 'class'
 }
