fbed642d80831e7f4e260fa046558a9b6ded1f04
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2022-12-21 11:13:39 -0500

    hide word in result when game not ended

--- src/lib/Result.svelte
+++ src/lib/Result.svelte
@@ -6,8 +6,11 @@
   import facebookSvg from '../assets/facebook.svg'
   import whatsappSvg from '../assets/whatsapp.svg'
   import twitterSvg from '../assets/twitter.svg'
+  import { gameOver } from '../lib/store'
 
   export let word: BibleWord
+
+  let isSecret: boolean = true
 
   const share = () => {
     navigator.share({
@@ -46,20 +49,28 @@
   <div class="grow flex flex-col justify-center items-center gap-7 max-w-full">
     <Bibdle withConfetti />
     <h2>Respuesta</h2>
-    <div class="space-y-3 max-w-full lg:max-w-none">
-      <h3>La palabra es</h3>
-      <div class="flex gap-2 max-w-full overflow-x-auto">
-        {#each word.name as key}
-          <Tile {key} matched />
-        {/each}
+    {#if !isSecret || $gameOver}
+      <div class="space-y-3 max-w-full lg:max-w-none">
+        <h3>La palabra es</h3>
+        <div class="flex gap-2 max-w-full overflow-x-auto">
+          {#each word.name as key}
+            <Tile {key} matched />
+          {/each}
+        </div>
       </div>
-    </div>
-    <div class="space-y-3">
-      <h3>Descripción</h3>
-      <div class="bg-lavenderGray/60 dark:bg-onyx/60 rounded-3xl py-5 px-7">
-        <p>{word.description}</p>
+      <div class="space-y-3">
+        <h3>Descripción</h3>
+        <div class="bg-lavenderGray/60 dark:bg-onyx/60 rounded-3xl py-5 px-7">
+          <p>{word.description}</p>
+        </div>
       </div>
-    </div>
+    {:else}
+      <div class="flex flex-col items-center">
+        <p>Aún no has terminado de jugar</p>
+        <p>¿Quieres revelar la palabra?</p>
+        <button on:click={() => isSecret = false} class="rounded-full px-5 py-3 bg-lightGreen hover:bg-green dark:bg-darkGreen font-black mt-3">REVELAR</button>
+      </div>
+    {/if}
     <div class="space-y-3">
       <h2>Compartir</h2>
       <div class="flex justify-center gap-3">
